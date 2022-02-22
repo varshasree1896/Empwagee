2b12083d98e7e7d2a760fdd53394d82a14fe0245
Pooja Sree <you@example.com>
2022-02-22 23:53:29 +0530

    Added:employeeattendance

--- src/com/blz/empwagee/Empwagee.java
+++ src/com/blz/empwagee/Empwagee.java
@@ -3,5 +3,12 @@ package com.blz.empwagee;
 public class Empwagee {
     public static void main(String[] args) {
         System.out.println("Welcome to Employee wage");
+        //Use random function to check employee is present or absent
+        int empCheck = (int) (Math.floor(Math.random() * 10) % 2);
+        if ( empCheck == 1 ) {
+            System.out.println("Employee is Present");
+        } else {
+            System.out.println("Employee is Absent");
+        }
     }
 }
