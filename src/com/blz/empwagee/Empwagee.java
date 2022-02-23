c2ac39e78352af3110b0a01e96d4ac65cd684aac
Pooja Sree <you@example.com>
2022-02-23 23:16:19 +0530

    Added:daily wage calculation file

--- src/com/blz/empwagee/Empwagee.java
+++ src/com/blz/empwagee/Empwagee.java
@@ -5,10 +5,13 @@ public class Empwagee {
         System.out.println("Welcome to Employee wage");
         //Use random function to check employee is present or absent
         int empCheck = (int) (Math.floor(Math.random() * 10) % 2);
-        if ( empCheck == 1 ) {
+        if (empCheck == 1) {
             System.out.println("Employee is Present");
         } else {
             System.out.println("Employee is Absent");
         }
     }
 }
+
+
+
